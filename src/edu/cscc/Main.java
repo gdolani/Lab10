e70eb038c8557fefcd5822c844b7a57011702a34
gdolani <gduresso@student.cscc.edu>
2020-01-07 17:39:40 -0500

    Initial commit

---
+++ src/edu/cscc/Main.java
@@ -0,0 +1,77 @@
+package edu.cscc;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+     //Name: Girma Duresso, Date: 11/14/2019.
+    //The purpose of this program is to write an application of Student data sorting by Last name, Major, zipcode, and GPA.
+    public static void main(String[] args) {
+        Student[] students = TestData.createStudents();
+        Arrays.sort(students,new Comparator<Student>() {
+            public int compare(Student s1,Student s2) {
+                String lastname1 = s1.getLastName();
+                String lastname2 = s2.getLastName();
+                return lastname1.compareTo(lastname2);
+            }
+        });
+        printStudentList("Students Sorted By LastName",students);
+
+        // TODO - sort students by major
+
+        Arrays.sort(students, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                String major1 = s1.getMajor();
+                String major2 = s2.getMajor();
+                return major1.compareTo(major2);
+            }
+        });
+        printStudentList("Students Sorted By Major",students);
+
+        // TODO - sort students by zip code
+
+        Arrays.sort(students, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+
+                int zipcode1 = s1.getZipcode();
+                int zipcode2 = s2.getZipcode();
+                if (zipcode1 == zipcode2)
+                    return 0;
+                else if (zipcode1 > zipcode2)
+                    return 1;
+                else
+                    return -1;
+
+            }
+        });
+        printStudentList("Students Sorted By Zip Code",students);
+
+        // TODO - sort students by GPA
+        Arrays.sort(students, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                double gpa;
+                double gpa1 = s1.getGpa();
+                double gpa2 = s2.getGpa();
+                if (gpa1 == gpa2)
+                    return 0;
+                else if (gpa1 < gpa2)
+                    return 1;
+                else
+                    return -1;
+
+            }
+        });
+
+        printStudentList("Students Sorted By GPA",students);
+    }
+
+    public static void printStudentList(String title,Student[] list) {
+        final String format = "%-12s %-12s %-18s %-12s %-12s\n";
+        System.out.println(title);
+        System.out.printf(format,"First Name","Last Name","Major","Zip Code","GPA");
+        for (Student s : list) {
+            System.out.printf(format,s.getFirstName(),s.getLastName(),s.getMajor(),s.getZipcode(),s.getGpa());
+        }
+        System.out.println("==============================================================\n");
+    }
+}
